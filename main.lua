7beaa690f034fca0f64d8cecb5c1f87950745bd2
Jacob Smith <jacobwillsmith@gmail.com>
2014-10-13 16:14:52 -0700

    Added empty lua files for different objects needed documentation in main.lua for functionality needed

--- main.lua
+++ main.lua
@@ -2,13 +2,24 @@
 -- AnAL is a animation library
 require("libs/AnAL")
 -- player is an object library
+require("objects/camera")
+require("objects/collision")
+require("objects/data")
+require("objects/objectmap")
 require("objects/player")
+require("objects/spawner")
+require("objects/tilemap")
+require("objects/window")
+require("objects/zbuffer")
 
 -- runs when game is loaded
 function love.load()
 
 	-- call player load function
 	player.load()
+
+	tile_size = 32;
+	-- TODO: tile size variable
 
 	-- create explosion that loops
 	explosion = love.graphics.newImage("images/explosion.png")
@@ -17,6 +28,42 @@ function love.load()
 	-- create explosion that bounces
 	anim_explosion_bounce = newAnimation( explosion, 96, 96, 0.1, 0 )
 	anim_explosion_bounce:setMode("bounce")
+
+
+	-- TODO: generate level
+	--[[
+		world holds level for each floor
+		level holds variable that has grid of level (tiles)
+			tiles for (wall, floor, corners)
+		grid of objects (chests) and enemies (positions at start)
+			names are stored at location
+		]]
+
+	-- TODO: have database object that holds enemy, item and attack data
+	-- 		define variables for each object in database
+	-- 		be able to look up data from/for objects, give object name, and variable to find value
+
+	-- TODO: have window for player with width and height, and tracks (resolution / tile size ), x position, y positions,
+	--		window 
+	-- TODO: have camera with width and height (resolution), and tracks, xposition, yposition ( similar to window )
+	--  	as player moves, adjust camera, enemy, object, and tile spawner
+	--		removes objects farther than where they are spawned
+	-- TODO: spawner object, checks map for new objects, enemes, tiles as camera scans the maps
+
+	-- TODO: zbuffer list, holds an elements for each y pixel
+	--		as camera moves, lists are updated, list position is object yorigin position - camera yposition
+	--		when objects are drawn, they are drawn in order of the zbuffer, y-resolution -> zero. Draw event loops through objects
+
+	-- TODO: colliion overlord object: database and functions for different collisons
+	-- 		transfers data from object to other object. Like damage to health
+
+	-- TODO: create sound manager object, that takes sounds and plays them
+
+	-- TODO: create music manager object, that takes songs and plays them
+
+	-- TODO: player UI object, show health, updated when player is hit
+
+	-- TODO: card UI object, shows current hand, gets cards added when drawing, gets cards removed when abilities used
 
 end
 
@@ -30,6 +77,16 @@ function love.update(dt)
 	-- update explosion animations
 	anim_explosion:update(dt)   
 	anim_explosion_bounce:update(dt)  
+
+
+	-- TODO: update window and camera
+
+	-- TODO: check window bounds with spawners, add to scene object. loop through data map,
+
+	-- TODO: update all objects in scene. loop through zbuffer
+
+	-- TODO: resolve any collisions, check objects with zbuffer
+
 end
 
 -- runs when game is drawn, every step
@@ -41,4 +98,8 @@ function love.draw()
 	-- update explosion animations
 	anim_explosion:draw(200,100)
 	anim_explosion_bounce:draw(300,100)
+
+	-- loop through zbuffer and draw objects
+
+
 end
--- objects/player.lua
+++ objects/player.lua
@@ -11,13 +11,16 @@ function player.load()
 	player.y = 100
 	player.speed = 120
 	player.img = love.graphics.newImage("images/square.png")
+	player.attack_cooldown = 0
 end
 
 function player.update(dt)
 	
+	if love.keyboard.isDown(" ") or player.attack_cooldown > 0 then
+		player.attack(dt)
 	--move player
-	if (player.state == "stop") or (player.state == "walk") then
-			player.move(player.speed * dt)
+	elseif love.keyboard.isDown("left") or love.keyboard.isDown("right") or love.keyboard.isDown("up") or love.keyboard.isDown("down") then
+		player.move(player.speed * dt)
 	end
 
 end
@@ -26,8 +29,12 @@ function player.stop()
 	
 end
 
-function player.attack()
-	
+function player.attack(dt)
+	if player.attack_cooldown <= 0 then
+		player.attack_cooldown = 0.5
+	else
+		player.attack_cooldown = player.attack_cooldown - dt;
+	end
 end
 
 function player.move(speed)
@@ -35,22 +42,29 @@ function player.move(speed)
 	local ymove = 0
 
 	-- xmovement, left key cancels out right key and vice versa
-	if (love.keyboard.isDown("left")) then
+	if love.keyboard.isDown("left") then
 		xmove = xmove - 1
 	end
-	if (love.keyboard.isDown("right")) then
+	if love.keyboard.isDown("right") then
 		xmove = xmove + 1
 	end
 	player.x = player.x + (xmove * speed)
 
 	-- ymovement, up key cancels out down key and vice versa
-	if (love.keyboard.isDown("up")) then
+	if love.keyboard.isDown("up") then
 		ymove = ymove - 1
 	end
-	if (love.keyboard.isDown("down")) then
+	if love.keyboard.isDown("down") then
 		ymove = ymove + 1
 	end
 	player.y = player.y + (ymove * speed)
+
+	-- change state
+	if ( xmove == 0 and ymove == 0) then
+		player.state = "stop"
+	else 
+		player.state = "walk"
+	end
 	
 end
 
